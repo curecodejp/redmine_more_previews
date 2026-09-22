f983574255e4a4363d1fdd6de476a82feb8e27a6
Fumitaka TSUCHIDA <tsuchida@curecode.jp>
2026-09-22 16:51:10 +0900

    Test CSP as sent on HTML preview responses

--- test/unit/html_preview_csp_test.rb
+++ test/unit/html_preview_csp_test.rb
@@ -3,8 +3,22 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class HtmlPreviewCspTest < ActiveSupport::TestCase
-  def test_same_origin_styles_and_images_are_allowed_without_restoring_same_origin
-    csp = RedmineMorePreviews::ControllerHelper::HTML_PREVIEW_CSP
+  def test_html_preview_response_allows_same_origin_styles_and_images_without_restoring_same_origin
+    controller = Class.new do
+      include RedmineMorePreviews::ControllerHelper
+
+      attr_reader :params, :response
+
+      def initialize(format)
+        @params = ActionController::Parameters.new(format: format)
+        @response = ActionDispatch::TestResponse.new
+      end
+
+      public :apply_preview_security_headers
+    end.new('html')
+
+    controller.apply_preview_security_headers
+    csp = controller.response.headers['Content-Security-Policy'].to_s
 
     assert_includes csp, 'sandbox;'
     assert_not_includes csp, 'allow-same-origin'
